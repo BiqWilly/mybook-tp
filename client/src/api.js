ec0c41ce2d7f621f5aa0768166fc106aa51142a6
BiqWilly <williampotatohz@gmail.com>
2026-01-22 20:24:30 +0800

    Updated api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // client/src/api.js
 const API_URL = import.meta.env.MODE === 'development' 
   ? "http://localhost:5000" 
-  : "https://mybooktp-rnze5aqz0-williams-projects-9f6f755b.vercel.app"; 
+  : "https://mybooktp.vercel.app"; 
 
 export default API_URL;
