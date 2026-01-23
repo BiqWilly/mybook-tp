37af46bad9fb97e3ba4a328db83fab8922446c97
BiqWilly <williampotatohz@gmail.com>
2026-01-23 22:35:32 +0800

    added the correct frontend URL in api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // client/src/api.js
 const API_URL = import.meta.env.MODE === 'development' 
   ? "http://localhost:5000" 
-  : "https://mybooktp.vercel.app"; 
+  : "https://mybook-backend-glec.onrender.com/"; 
 
 export default API_URL;
