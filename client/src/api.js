94a6eb8a3a164570f8159b05399e7f8880ae7f63
BiqWilly <williampotatohz@gmail.com>
2026-01-23 23:35:21 +0800

    there was some double slash action for the URL, removed a slash in api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // client/src/api.js
 const API_URL = import.meta.env.MODE === 'development'
   ? "http://localhost:5000" 
-  : "https://mybook-backend-glec.onrender.com/"; 
+  : "https://mybook-backend-glec.onrender.com"; 
 
 export default API_URL;
