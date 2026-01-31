5b1a91284826d30c19a399b9f2d4d2691564ebe8
BiqWilly <williampotatohz@gmail.com>
2026-01-31 14:47:53 +0800

    checking to see if CD is working for backend

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get('/api', (req, res) => {
 const PORT = process.env.PORT || 5000; // fallback for local dev
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running, port ${PORT}`);
 });
 
 module.exports = app;
